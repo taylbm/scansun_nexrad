df2cb36663345e7b8c0ea7f78a4a3c657f2b1f47
root <root@ip-10-101-0-78.ec2.internal>
2020-10-27 14:53:14 +0000

    add output with time window of -1hr +2hr -bt

--- query_nexrad.py
+++ query_nexrad.py
@@ -85,8 +85,7 @@ for site in sites_sliced:
                 first_today = False
             sr_diff = (today_sr - file_dt).total_seconds()
             ss_diff = (today_ss - file_dt).total_seconds()
-            if -10800 < sr_diff < 0 or 0 < ss_diff < 10800:
-                print(sr_diff, ss_diff)
+            if -7200 < sr_diff < 3600 or -3600 < ss_diff < 7200:
                 if not os.path.exists(ROOT_PATH + key_path):
                     os.makedirs(ROOT_PATH + key_path)
                 if not os.path.exists(ROOT_PATH + key):
